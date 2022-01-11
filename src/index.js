c4b26b51ac121c48c15b83f88eca839abd963003
Omar <omarsalem0721@gmail.com>
2022-01-11 14:31:49 +0200

    Setup linetrs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import './style.css';
 
-let todosList = [
-    { description: 'todo2', completed: false, index: 2 },
-    { description: 'todo1', completed: false, index: 1 }
-]
+const todosList = [
+  { description: 'todo2', completed: false, index: 2 },
+  { description: 'todo1', completed: false, index: 1 },
+];
 
 const render = (list) => {
-    console.log(list)
-    const sortedTodos = list.sort((a, b) => a.index - b.index);
-    const todosContainer = document.querySelector('.todos');
-    let todosHtml = '';
-    sortedTodos.forEach((todo) => {
-        todosHtml += ` <div class="todo-item">
+  const sortedTodos = list.sort((a, b) => a.index - b.index);
+  const todosContainer = document.querySelector('.todos');
+  let todosHtml = '';
+  sortedTodos.forEach((todo) => {
+    todosHtml += ` <div class="todo-item">
         <input type="checkbox" /><span> ${todo.description}</span>
     </div>`;
-    });
-    todosContainer.innerHTML = todosHtml;
-}
+  });
+  todosContainer.innerHTML = todosHtml;
+};
 
 render(todosList);

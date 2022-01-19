3407079fafcf5b40ab98d4f5ff5fccc12d3bef50
Omar <omarsalem0721@gmail.com>
2022-01-19 15:40:44 +0200

    Add test function

---
+++ src/__mock__/todos.test.js
@@ -0,0 +1,46 @@
+/**
+ * @jest-environment jsdom
+ */
+import Todos from '../todos';
+
+document.body.innerHTML = `
+ <div class="container">
+ <h1 class="title">Today's Todo</h1>
+ <form>
+     <input class="input-todo" type="text" placeholder="Add to your List..." autofocus />
+     <button class="add-btn">
+         <i class="fas fa-location-arrow"></i>
+     </button>
+ </form>
+ <div class="todos">
+     <!-- Add Todos Automatically -->
+ </div>
+ <div class="clear">
+     <a class="clear-btn">Clear All</a>
+ </div>
+ </div>
+  `;
+describe('add and remove', () => {
+  // mock Localstorage
+  window.localStorage = Storage.prototype;
+  test('Add task', () => {
+    const todoList = new Todos();
+    const newTodo = {
+      id: 'id123',
+      description: 'task1',
+      completed: false,
+      index: 1,
+    };
+    const newTodo2 = {
+      id: 'id12sdad3',
+      description: 'task2',
+      completed: false,
+      index: 2,
+    };
+    todoList.addTodo(newTodo);
+    expect(todoList.list).toHaveLength(1);
+    todoList.addTodo(newTodo2);
+    expect(todoList.list).toHaveLength(2);
+    expect(todoList.list[1].description).toBe('task2');
+  });
+});
